894b93aea5eee9872cdfa4f754a5720a1a45fff3
Kelvin <anonymous@nowhere.geek.nz>
2022-12-03 12:59:56 +1300

    improve day1-1

--- 01/day1-1.py
+++ 01/day1-1.py
@@ -2,19 +2,19 @@
 
 file=list()
 
-with open("./input.txt", "r") as f:
-	file=f.read().splitlines()
-
 elves=list()
 
 elf=0
 elves.append(0)
 
-for line in file:
-	if len(line) > 0:
-		elves[elf] += int(line)
-	else:
-		elf+=1
-		elves.append(0)
+with open("./input.txt", "r") as f:
+	for line in map(str.strip, f):
+		if len(line) > 0:
+			elves[elf] += int(line)
+		else:
+			elf+=1
+			elves.append(0)
 
-print(sorted(elves)[-1])
+answer = sorted(elves)[-1]
+
+print(answer)
