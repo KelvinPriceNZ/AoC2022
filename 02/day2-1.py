17075ef2d6a9a698b96e24a7066da1ffb064c832
Kelvin <anonymous@nowhere.geek.nz>
2022-12-03 13:09:47 +1300

    day2-1 improvements

--- 02/day2-1.py
+++ 02/day2-1.py
@@ -17,39 +17,19 @@ RPS = {
 SCORE = {
 				"R" : 1,
 				"P" : 2,
-				"S" : 3,
-				"W" : 6,
-				"D" : 3,
-				"L" : 0
+				"S" : 3
 			}
 
 def Result(them, me):
-	if them == "R" and me == "R":
-		return 3
-
-	if them == "R" and me == "P":
-		return 6
-
-	if them == "R" and me == "S":
-		return 0
-
-	if them == "P" and me == "R":
-		return 0
-
-	if them == "P" and me == "P":
-		return 3
-
-	if them == "P" and me == "S":
-		return 6
-
-	if them == "S" and me == "R":
-		return 6
-
-	if them == "S" and me == "P":
-		return 0
-
-	if them == "S" and me == "S":
-		return 3
+	if them == "R" and me == "R": return 3
+	if them == "R" and me == "P": return 6
+	if them == "R" and me == "S": return 0
+	if them == "P" and me == "R": return 0
+	if them == "P" and me == "P": return 3
+	if them == "P" and me == "S": return 6
+	if them == "S" and me == "R": return 6
+	if them == "S" and me == "P": return 0
+	if them == "S" and me == "S": return 3
 
 total_score = 0
 
@@ -59,7 +39,5 @@ for line in file:
 	me = RPS[me]
 	score = Result(elf, me) + SCORE[me]
 	total_score += score
-	print(f"elf {elf} me {me} score: {score}")
-
 
 print(total_score)
