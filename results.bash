d0d7273fbb972ea9327c6502a5d0744214c5e642
Kelvin <anonymous@nowhere.geek.nz>
2022-12-03 14:39:32 +1300

    add results script

---
+++ results.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for day in [012][0-9]
+do
+   cd $day
+
+   echo -n "Day $(basename $day)"
+
+   solved=0
+   for part in 1 2
+   do
+      soln="./day${day}-${part}.py"
+      echo -en "\tPart $part"
+      if [ -s ${soln} ]
+      then
+         echo -en "\t\t"
+         $soln
+      else
+         echo -en "\t\t*Unsolved*"
+         echo ""
+      fi
+   done
+   cd ~-
+done
