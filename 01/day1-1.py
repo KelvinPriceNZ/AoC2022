a72a6155d311a8813dd3967bff2a45c21fb90806
Kelvin <anonymous@nowhere.geek.nz>
2022-12-03 14:02:58 +1300

    vim retabs

--- 01/day1-1.py
+++ 01/day1-1.py
@@ -8,12 +8,12 @@ elf=0
 elves.append(0)
 
 with open("./input.txt", "r") as f:
-	for line in map(str.strip, f):
-		if len(line) > 0:
-			elves[elf] += int(line)
-		else:
-			elf+=1
-			elves.append(0)
+   for line in map(str.strip, f):
+      if len(line) > 0:
+         elves[elf] += int(line)
+      else:
+         elf+=1
+         elves.append(0)
 
 answer = sorted(elves)[-1]
 
