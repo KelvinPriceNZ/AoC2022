f1b427c6e9555f34a2c791a73d122875d4642729
Kelvin <anonymous@nowhere.geek.nz>
2022-12-04 19:12:36 +1300

    add skeleton python script, add mkday.bash to copy skeleton (x2) to $1 folder and run fetch-input

---
+++ dayxx-y.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+lines = list()
+
+with open("./input.txt", "r") as f:
+   lines=f.read().splitlines()
+
+for line in lines:
+   pass
+
+print("*Unsolved*")
---
+++ mkday.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d $1 ]
+then
+   cp -p dayxx-y.py $1/day{$1}-1.py
+   cp -p dayxx-y.py $1/day{$1}-2.py
+fi
+
+cd $1
+
+../fetch-input
