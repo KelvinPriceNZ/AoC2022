08f4b2bc68e65cd2bc6bbdc13cbbd98e4a40573d
Kelvin <anonymous@nowhere.geek.nz>
2022-12-05 19:06:27 +1300

    fix typo in mkday.bash "{$" should have been "${"

--- mkday.bash
+++ mkday.bash
@@ -2,8 +2,8 @@
 
 if [ -d $1 ]
 then
-   cp -p dayxx-y.py $1/day{$1}-1.py
-   cp -p dayxx-y.py $1/day{$1}-2.py
+   cp -p dayxx-y.py $1/day${1}-1.py
+   cp -p dayxx-y.py $1/day${1}-2.py
 
    cd $1
 
