d416f6ce71b22c54aaf1a511348172ce39c485e9
Kelvin <anonymous@nowhere.geek.nz>
2022-12-07 23:16:00 +1300

    Day 7 answers

---
+++ 07/day07-1.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+class DirTree:
+
+   def __init__(self, name):
+      self.name = name
+      self.size = 0
+      self.files = list()
+      self.dirs = dict()
+      self.parent = None
+
+   def get_size(self):
+      total = 0
+      for file in self.files:
+         total += file.size
+      for name, dir in self.dirs.items():
+         total += dir.get_size()
+      self.size = total
+      return total
+
+   def __repr__(self):
+      me = ""
+      for file in self.files:
+         me += str(file) + "\n"
+      for dir in self.dirs.values():
+         me += f"{dir.name}: {dir.size}\n"
+      return me
+
+class File:
+
+   def __init__(self, name, size):
+      self.name = name
+      self.size = size
+
+   def __repr__(self):
+      return f"{self.name}: {self.size}\n"
+
+lines = list()
+
+with open("./input.txt", "r") as f:
+   lines=f.read().splitlines()
+
+root = DirTree("/")
+cwd="/"
+for line in lines:
+   fields = line.split()
+
+   if fields[0] == "$":
+      if fields[1] == "cd":
+         if fields[2] == "/":
+            cwd = "/"
+            tree = root
+         elif fields[2] == "..":
+            nodes = cwd.split("/")[1:]
+            nodes.pop()
+            if len(nodes) == 0:
+               cwd = "/"
+            elif len(nodes) == 1:
+               cwd = "/" + nodes[0]
+            else:
+               cwd = "/" + "/".join(nodes)
+            if cwd == "/":
+               tree = root
+            else:
+               tree = tree.parent
+         else:
+            if cwd[-1] != "/":
+               cwd += "/"
+            cwd += fields[2]
+            parent_tree = tree
+            tree = tree.dirs[fields[2]]
+            #print(cwd)
+
+   else:
+      if fields[0] == "dir":
+         name = fields[1]
+         tree.dirs[name]=(DirTree(name))
+         tree.dirs[name].parent=tree
+         #print(f"adding dir {name} to {tree.name}")
+      else:
+         size = int(fields[0])
+         name = fields[1]
+         file = File(name, size)
+         tree.files.append(file)
+         #print(f"adding file {name} to {tree.name}")
+
+#print("Files")
+#for file in root.files:
+   #print(file)
+
+#print("Size")
+for name, dir in root.dirs.items():
+   dir.get_size()
+
+targets=list()
+#print("Dirs")
+def print_dirs(dirtree):
+   for dir in dirtree.dirs.values():
+      #print(dir)
+      print_dirs(dir)
+      if dir.size <= 100_000:
+         targets.append(dir.size)
+
+print_dirs(root)
+print(sum(targets))
