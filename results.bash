5e046097b8b94da4702673a4fc72bfa048ac7f5b
Kelvin <anonymous@nowhere.geek.nz>
2022-12-10 20:12:26 +1300

    Change output format of results.bash because Day 10 - part 2 was multi-line output

--- results.bash
+++ results.bash
@@ -4,18 +4,15 @@ for day in {01..25}
 do
    cd $day
 
-   echo -n "Day $(basename $day)"
-
    for part in 1 2
    do
       soln="./day${day}-${part}.py"
-      echo -en "\tPart $part"
+      echo -e "Day $(basename $day) Part $part"
       if [ -s ${soln} ]
       then
-         echo -en "\t\t"
          $soln
       else
-         echo -e "\t\t*Unsolved*"
+         echo -e "*Unsolved*"
       fi
    done
    cd ~-
