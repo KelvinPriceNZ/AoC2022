17ac761e49f500ab4d777c4f4b0a8ce17d816107
Kelvin <anonymous@nowhere.geek.nz>
2022-12-11 13:54:48 +1300

    Day 10 part 2 - improvements

--- 10/day10-2.py
+++ 10/day10-2.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import math
 
+symbol = {
+   True:  "█",
+   False: " "
+}
+
 lines = list()
 
 with open("./input.txt", "r") as f:
@@ -9,21 +14,20 @@ with open("./input.txt", "r") as f:
 X = 1
 
 cycle = 0
-print("█", end="")
 
 def tick():
    global X
    global cycle
 
-   cycle += 1
-
    pixel = cycle % 40
 
-   pixel_on = abs(X - pixel) < 2 
+   pixel_on = abs(pixel - X) < 2
 
-   print("█" if pixel_on else " ", end="")
+   print(f"{symbol[pixel_on]}", end="")
 
    if pixel == 39 : print()
+
+   cycle += 1
 
 for line in lines:
    tick()
@@ -33,7 +37,7 @@ for line in lines:
    if instr == "noop":
       pass
    elif instr == "addx":
+      tick()
+
       amount = int(line[5:])
       X += amount
-
-      tick()
